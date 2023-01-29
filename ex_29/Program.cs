f21efe64e3159a8cc827c6d81e0c5011d5d75449
Dmitriy Kurban <d_kurban@yahoo.com>
2023-01-29 18:20:41 +1000

    ex_29 is done

--- ex_29/Program.cs
+++ ex_29/Program.cs
@@ -2,3 +2,40 @@
 
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33] */
+
+void PrintArr(int [] array)
+{
+    int k = array.Length;
+    for (int j = 0; j < k; j++)
+    {
+        if (j == k-1)
+        {
+            Console.Write($"{array[j]}");
+        }
+        else
+        {
+            Console.Write($"{array[j]},");
+        }
+    }
+}
+
+void NewArray(int [] array)
+{
+    // int [] arr = new int [8];
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+}
+
+//делаем массив из 8-ми эл-ов
+int [] arr = new int [8];
+NewArray(arr);
+
+//Выводим ана печать каждый элемент
+PrintArr(arr);
+
+
+
+
