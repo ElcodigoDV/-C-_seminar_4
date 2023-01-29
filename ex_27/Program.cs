cbaabc0d99ff1b89975d7954df8511d7b1589a1d
Dmitriy Kurban <d_kurban@yahoo.com>
2023-01-29 17:12:32 +1000

    ex_27 is done

--- ex_27/Program.cs
+++ ex_27/Program.cs
@@ -4,3 +4,24 @@
 82 -> 10
 9012 -> 12 */
 
+int Sum(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number = number /10;
+    }
+    return sum;
+}   
+
+int GetNum(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int num = GetNum("Введите число");
+int summa = Sum(num);
+Console.WriteLine($"{num} -> {summa}");
